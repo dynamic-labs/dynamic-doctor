c10a7c21510d72e1ff8d3e5619cf7b24c74db931
bartkownacki <bartosz.kownacki@sofomo.com>
2023-07-14 10:12:33 +0200

    chore: create dynamic doctor config

---
+++ README.md
@@ -0,0 +1,11 @@
+# doctor
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build doctor` to build the library.
+
+## Running unit tests
+
+Run `nx test doctor` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'doctor',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  resetMocks: false,
+  maxWorkers: '2',
+  workerIdleMemoryLimit: '2.25gb',
+  preset: 'ts-jest',
+};
