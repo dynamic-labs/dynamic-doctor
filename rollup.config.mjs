b6dae8581c4550bf03c57a4b1811fe80802cb3ea
bartkownacki <bartosz.kownaki@sofomo.com>
2023-08-24 13:59:21 +0200

    chore: basic rollup config

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
+import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'bin/index.ts',
+  output: {
+    sourcemap: true,
+    file: 'dist/index.js',
+    format: 'cjs',
+  },
+  external: [
+    'commander',
+    'enquirer',
+    'node-fetch',
+    'path',
+    'pug',
+    'fs',
+    'child_process',
+    'chalk',
+    'fs/promises',
+  ],
+  plugins: [
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    preserveShebangs(),
+    copy({
+      targets: [{ src: 'src/templates/*', dest: 'dist/src/templates' }],
+    }),
+  ],
+};
